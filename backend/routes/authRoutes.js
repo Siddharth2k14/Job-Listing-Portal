484fb2a451d95f5f734647621fdb6467f3f8be27
Madhubala.S <147246282+16Madhubala@users.noreply.github.com>
2025-12-11 18:58:45 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,3 @@
-// import express from "express";
-// import { signupUser, loginUser, verifyOtp } from "../controllers/authController.js"
-
-// const router = express.Router();
-
-// router.post("/signup", signupUser);
-// router.post("/login", loginUser);
-// router.post("/verify-otp", verifyOtp);
-
-// export default router;
-
 import express from 'express';
 import { signupUser, loginUser } from '../authController.js';
 
@@ -18,3 +7,4 @@ router.post('/signup', signupUser);
 router.post('/login', loginUser);
 
 export default router;
+
